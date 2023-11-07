a6097ded07e2fb38dec8769dada8032f16fe5478
evi1Husky <evi1husky0@gmail.com>
2023-11-07 11:06:10 +1000

    add manifest and icon

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,29 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,woff2}"],
+      },
       registerType: "autoUpdate",
       devOptions: {
         enabled: true,
+      },
+      manifest: {
+        theme_color: "rgb(18, 22, 33)",
+        background_color: "rgb(18, 22, 33)",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "just breathe",
+        description: "breathing exercise app",
+        name: "just breathe",
+        icons: [
+          {
+            src: "/appicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
